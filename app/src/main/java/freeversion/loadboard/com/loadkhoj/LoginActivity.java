3c976a3624e7cd5d578bd46931bd291f819fae92
amit.ja <blueeyeschild@gmail.com>
2015-06-20 16:37:06 +0530

    1. complete login 2. handled connection exception 3. handled json object exception

--- app/src/main/java/freeversion/loadboard/com/loadkhoj/LoginActivity.java
+++ app/src/main/java/freeversion/loadboard/com/loadkhoj/LoginActivity.java
@@ -12,12 +12,16 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -29,13 +33,17 @@ public class LoginActivity extends ActionBarActivity {
 
     public final static String loginEmailIdMessage = "freeversion.loadboard.com.loadkhoj.loginEmailIdMessage";
     public final static String loginPasswordMessage = "freeversion.loadboard.com.loadkhoj.loginPasswordMessage";
+    EditText loginEmailIdEditText;
+    EditText loginPasswordEditText;
     TextView responseTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        responseTextView = (TextView) findViewById(R.id.apiResponse);
+        responseTextView = (TextView) findViewById(R.id.responseTextview);
+        loginPasswordEditText = (EditText) findViewById(R.id.loginPassword);
+        loginEmailIdEditText = (EditText) findViewById(R.id.loginEmailId);
     }
 
 
@@ -45,28 +53,6 @@ public class LoginActivity extends ActionBarActivity {
         getMenuInflater().inflate(R.menu.menu_load_khoj_main, menu);
         StrictMode.enableDefaults();
         return true;
-    }
-
-    public void startRegisteration(View view) {
-        Intent registerNewUserIntent = new Intent(this, RegisterPartOne.class);
-        //starting registration activity
-        startActivity(registerNewUserIntent);
-    }
-
-    public void loginSuccessful(View view) {
-        checkConnection();
-        /*Intent loadKhojMainActivityIntent = new Intent(this, LoadKhojMainActivity.class);
-        //taking input from layout
-        EditText loginEmailIdEditText = (EditText) findViewById(R.id.loginEmailId);
-        EditText loginPasswordEditText = (EditText) findViewById(R.id.loginPassword);
-        //assigning value to variable
-        String loginEmailId = loginEmailIdEditText.getText().toString();
-        String loginPassword = loginPasswordEditText.getText().toString();
-        //passing it to next activity
-        loadKhojMainActivityIntent.putExtra(loginEmailIdMessage,loginEmailId);
-        loadKhojMainActivityIntent.putExtra(loginPasswordMessage,loginPassword);
-        //starting successful Login activity
-        startActivity(loadKhojMainActivityIntent);*/
     }
 
     @Override
@@ -84,24 +70,67 @@ public class LoginActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+
+    public void startRegisteration(View view) {
+        Intent registerNewUserIntent = new Intent(this, RegisterPartOne.class);
+        //starting registration activity
+        startActivity(registerNewUserIntent);
+    }
+
+    public void loginSuccessful(View view) {
+        checkConnection();
+    }
+
+
     public void checkConnection() {
         ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo netInfo = cm.getActiveNetworkInfo();
-        HttpAsyncTask myRequest = null;
+        LoginConnectionAsyncTask myRequest = null;
         if (netInfo != null && netInfo.isConnected()) {
-                myRequest = new HttpAsyncTask();
-                myRequest.execute("http://192.168.0.104/login.php");
+                myRequest = new LoginConnectionAsyncTask();
+                myRequest.execute("http://192.168.0.103/login.php");
         } else {
             responseTextView.setText("Net connection or wifi is disabled");
         }
     }
 
-    private class HttpAsyncTask extends AsyncTask<String, Void, String> {
+    public void loadIntent(){
+        Intent loadKhojMainActivityIntent = new Intent(this, LoadKhojMainActivity.class);
+
+        //assigning value to variable
+        String loginEmailId = loginEmailIdEditText.getText().toString();
+        String loginPassword = loginPasswordEditText.getText().toString();
+        //passing it to next activity
+        loadKhojMainActivityIntent.putExtra(loginEmailIdMessage,loginEmailId);
+        loadKhojMainActivityIntent.putExtra(loginPasswordMessage,loginPassword);
+        //starting successful Login activity
+        startActivity(loadKhojMainActivityIntent);
+    }
+
+    private class LoginConnectionAsyncTask extends AsyncTask<String, Void, String> {
         @Override
         protected String doInBackground(String... urls) {
             try {
                 URL url = new URL(urls[0]);
                 HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                httpURLConnection.setRequestProperty("Content-Type", "application/json");
+                httpURLConnection.setRequestProperty("Accept", "application/json");
+                httpURLConnection.setRequestMethod("POST");
+
+                JSONObject auth=new JSONObject();
+                try {
+                    auth.put("userName", loginEmailIdEditText.getText().toString());
+                    auth.put("password", loginPasswordEditText.getText().toString());
+                } catch (JSONException e) {
+                    Log.wtf("Json Decode Error", e.getMessage());
+                    return "Decode Error";
+                }
+                OutputStreamWriter wr= new OutputStreamWriter(httpURLConnection.getOutputStream());
+                wr.write(auth.toString());
+                wr.flush();
+                wr.close();
                 httpURLConnection.setConnectTimeout(5 * 1000);          // 5 s.
                 httpURLConnection.connect();
                 if (httpURLConnection.getResponseCode() == 200) // 200 = "OK" code (http connection is fine).
@@ -114,17 +143,39 @@ public class LoginActivity extends ActionBarActivity {
                     return "Connection Error";
                 }
             } catch (MalformedURLException e) {
-                Log.wtf("Login Activity Error",e.getMessage());
+                Log.wtf("Login Activity Error", e.getMessage());
+                return "Connection Error";
             } catch (IOException ioe) {
                 Log.wtf("Login Activity Error",ioe.getMessage());
+                return "Connection Error";
             }
-            return null;
         }
 
         // onPostExecute displays the results of the AsyncTask.
         @Override
         protected void onPostExecute(String result) {
-            responseTextView.setText("got response: "+result);
+//            responseTextView.setText("got response: "+result);
+            Log.wtf("response",result);
+            if(result.equals("Connection Error")){
+                Log.wtf("Json Output", result);
+                responseTextView.setText("Login Failed.Connection Error.");
+            }else if(result.equals("Decode Error")){
+                Log.wtf("Json Output", result);
+                responseTextView.setText("Login Failed.Decode Error.");
+            }
+            else {
+                try {
+                    JSONObject jObject = new JSONObject(result);
+//                if(jObject.getString("status").equals("true"))
+                    if (jObject.getBoolean("status"))
+                        loadIntent();
+                    else
+                        responseTextView.setText("Login Failed.");
+                } catch (JSONException e) {
+                    Log.wtf("Json Decode Error", e.getMessage());
+                    responseTextView.setText("Login Failed.Error.");
+                }
+            }
         }
     }
 }
