a85844ac9776e9ae5f5f5a96f774a2e8dd018f77
amit.ja <amit.ja@directi.com>
2015-07-27 00:12:19 +0530

    added JSONobject to transfer data between activities

--- app/src/main/java/freeversion/loadboard/com/loadkhoj/activities/RegisterPartOne.java
+++ app/src/main/java/freeversion/loadboard/com/loadkhoj/activities/RegisterPartOne.java
@@ -3,18 +3,24 @@ package freeversion.loadboard.com.loadkhoj.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import freeversion.loadboard.com.loadkhoj.R;
 
 
 public class RegisterPartOne extends Activity {
 
-    public final static String registerFullNameMessage = "freeversion.loadboard.com.loadkhoj.registerFullNameMessage";
-    public final static String registerMobileNumberMessage = "freeversion.loadboard.com.loadkhoj.registerMobileNumberMessage";
-    public final static String registerEmailIdMessage = "freeversion.loadboard.com.loadkhoj.registerEmailIdMessage";
-    public final static String registerPasswordMessage = "freeversion.loadboard.com.loadkhoj.registerPasswordMessage";
+//    public final static String registerFullNameMessage = "freeversion.loadboard.com.loadkhoj.registerFullNameMessage";
+//    public final static String registerMobileNumberMessage = "freeversion.loadboard.com.loadkhoj.registerMobileNumberMessage";
+//    public final static String registerEmailIdMessage = "freeversion.loadboard.com.loadkhoj.registerEmailIdMessage";
+//    public final static String registerPasswordMessage = "freeversion.loadboard.com.loadkhoj.registerPasswordMessage";
+
+    public final static String registerUserDataMessage = "registerPartOneUserData";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,20 +31,33 @@ public class RegisterPartOne extends Activity {
     public void registerPartTwo(View view){
         Intent registerPartTwoIntent = new Intent(this, RegisterPartTwo.class);
         //taking input from layout
-        EditText registerFullNameEditText = (EditText) findViewById(R.id.registerFullName);
-        EditText registerMobileNumberEditText = (EditText) findViewById(R.id.registerMobileNumber);
-        EditText registerEmailIdEditText = (EditText) findViewById(R.id.registerEmailId);
-        EditText registerPasswordEditText = (EditText) findViewById(R.id.registerPassword);
+        EditText fullNameEditText = (EditText) findViewById(R.id.registerFullName);
+        EditText emailIdEditText = (EditText) findViewById(R.id.registerEmailId);
+        EditText passwordEditText = (EditText) findViewById(R.id.registerPassword);
+
         //assigning value to variable
-        String registerFullName = registerFullNameEditText.getText().toString();
-        String registerMobileNumber = registerMobileNumberEditText.getText().toString();
-        String registerEmailId = registerEmailIdEditText.getText().toString();
-        String registerPassword = registerPasswordEditText.getText().toString();
-        //passing it to next activity
-        registerPartTwoIntent.putExtra(registerFullNameMessage,registerFullName);
-        registerPartTwoIntent.putExtra(registerMobileNumberMessage,registerMobileNumber);
-        registerPartTwoIntent.putExtra(registerEmailIdMessage,registerEmailId);
-        registerPartTwoIntent.putExtra(registerPasswordMessage,registerPassword);
+        String fullName = fullNameEditText.getText().toString();
+        String emailId = emailIdEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+
+        //Creating Json Object of data
+        JSONObject userData = new JSONObject();
+        try {
+            userData.put("FullName", fullName);
+            userData.put("EmailId", emailId);
+            userData.put("Password", password);
+        } catch (JSONException e) {
+            Log.wtf("JSONException while creating register one object: ", e.getMessage());
+        }
+
+        //passing it to next a//ctivity
+//        registerPartTwoIntent.putExtra(registerFullNameMessage,registerFul//lName);
+//        registerPartTwoIntent.putExtra(registerMobileNumberMessage,registerMobileN//umber);
+//        registerPartTwoIntent.putExtra(registerEmailIdMessage,registerEm//ailId);
+//        registerPartTwoIntent.putExtra(registerPasswordMessage,registerPa
+
+        registerPartTwoIntent.putExtra(registerUserDataMessage, userData.toString());
+
         //starting successful Login activity
         startActivity(registerPartTwoIntent);
     }
--- app/src/main/java/freeversion/loadboard/com/loadkhoj/activities/RegisterPartTwo.java
+++ app/src/main/java/freeversion/loadboard/com/loadkhoj/activities/RegisterPartTwo.java
@@ -3,7 +3,11 @@ package freeversion.loadboard.com.loadkhoj.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.util.Log;
+import android.widget.EditText;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import freeversion.loadboard.com.loadkhoj.R;
 
@@ -16,23 +20,58 @@ public class RegisterPartTwo extends Activity {
         setContentView(R.layout.activity_register_part_two);
         // Get the message from the intent
         Intent registerPartOneIntent = getIntent();
-        String registerFullName = registerPartOneIntent.getStringExtra(RegisterPartOne.registerFullNameMessage);
-        String registerMobileNumber = registerPartOneIntent.getStringExtra(RegisterPartOne.registerMobileNumberMessage);
-        String registerEmailId = registerPartOneIntent.getStringExtra(RegisterPartOne.registerEmailIdMessage);
-        String registerPassword = registerPartOneIntent.getStringExtra(RegisterPartOne.registerPasswordMessage);
+        String registerPartOneUserData = registerPartOneIntent.getStringExtra(RegisterPartOne.registerUserDataMessage);
+
+        //taking input from layout
+        EditText typeOfCompanyEditText = (EditText) findViewById(R.id.registerTypeOfCompany);
+        EditText companyNameEditText = (EditText) findViewById(R.id.registerCompanyName);
+        EditText stdEditText = (EditText) findViewById(R.id.registerSTD);
+        EditText landlineEditText = (EditText) findViewById(R.id.registerLandline);
+        EditText countryCodeEditText = (EditText) findViewById(R.id.registerCountryCode);
+        EditText mobileNumberEditText = (EditText) findViewById(R.id.registerMobileNumber);
+        EditText addressEditText = (EditText) findViewById(R.id.registerAddress);
+        EditText cityEditText = (EditText) findViewById(R.id.registerCity);
+
+        //assigning value to variable
+        String typeOfCompany = typeOfCompanyEditText.getText().toString();
+        String companyName = companyNameEditText.getText().toString();
+        String std = stdEditText.getText().toString();
+        String landline = landlineEditText.getText().toString();
+        String countryCode = countryCodeEditText.getText().toString();
+        String mobileNumber = mobileNumberEditText.getText().toString();
+        String address = addressEditText.getText().toString();
+        String city = cityEditText.getText().toString();
+
+        JSONObject userData;
+        try {
+            userData = new JSONObject(registerPartOneUserData);
+        } catch (JSONException e) {
+            Log.wtf("JSONException while decoding register one object: ", e.getMessage());
+            userData = new JSONObject();
+        }
+
+        try {
+            userData.put("typeOfCompany", typeOfCompany);
+            userData.put("companyName", companyName);
+            userData.put("std", std);
+            userData.put("landline", landline);
+            userData.put("countryCode", countryCode);
+            userData.put("mobileNumber", mobileNumber);
+            userData.put("address", address);
+            userData.put("city", city);
+        } catch (JSONException e) {
+            Log.wtf("JSONException while creating register two object: ", e.getMessage());
+        }
+
 
         // Create the text view
-        TextView textView = (TextView) findViewById(R.id.Rp2);
-        textView.setText("fullname: " + registerFullName + " no:" + registerMobileNumber + "email: " + registerEmailId + " Password:" + registerPassword);
+//        TextView textView = (TextView) findViewById(R.id.Rp2);
+//        textView.setText(registerPartOneUserData);
 
-        Intent registerPartThreeIntent = new Intent(this, RegisterPartThree.class);
-        //passing it to next activity
-//        registerPartThreeIntent.putExtra(registerFullNameMessage,registerFullName);
-//        registerPartThreeIntent.putExtra(registerMobileNumberMessage,registerMobileNumber);
-//        registerPartThreeIntent.putExtra(registerEmailIdMessage,registerEmailId);
-//        registerPartThreeIntent.putExtra(registerPasswordMessage,registerPassword);
-        //starting successful Login activity
-        startActivity(registerPartThreeIntent);
+//        Intent registerPartThreeIntent = new Intent(this, RegisterPartThree.class);
+//        //starting successful Login activity
+//        startActivity(registerPartThreeIntent);
+
     }
 
 }
